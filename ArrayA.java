82f17b9b5ac4281aa3649f8ab31f95872f5a77ef
Rahul Raj <65505642+RahulRaajj@users.noreply.github.com>
2020-12-24 09:47:13 +0530

    Update ArrayA.java

--- ArrayA.java
+++ ArrayA.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 class ArrayA{
 	public static void main(String[] args) {
 		System.out.println("<<<<<=======Program starts======================>>>>>>>>");
+		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the size of the array");
 		int size=sc.nextInt();
 		//System.out.println("You have choosen the size of the array "+size);
 		int arr []=new int[size];
+		
         for (int i=0;i<arr.length ;i++ ) {
         	System.out.println("Enter the values you want in array at index "+i );
         	int index=sc.nextInt();
         	arr[i]=index;
         }
         System.out.println("You have selected the values below- ");
+		
         for (int i=0;i<arr.length ;i++ ) {
         	System.out.println(arr[i]);        	
         }
+		
         int max= arr[0];   // To find the highest number in the array
         for (int i=0;i<arr.length ;i++ ) {
         	if (max<arr[i]) {
@@ -23,6 +27,7 @@ class ArrayA{
         	}        	
         }
         System.out.println("The maximum number in the given array is "+max);
+		
 
         int min=arr[0];     // To find the lowest number in the array
         for (int i=0;i<arr.length ;i++ ) {
@@ -31,6 +36,7 @@ class ArrayA{
         	}        	
         }
         System.out.println("Minimum number in the given array is "+min);
+		
 
         int maximum=arr[0];
         int sec=0;
